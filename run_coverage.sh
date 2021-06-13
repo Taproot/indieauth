5ee97182bc54e81018f066a1bbda70b639c2dd6a
Barnaby Walters <barnaby@waterpigs.co.uk>
2021-06-13 15:47:54 +0200

    Regenerated coverage to work with GH pages:

--- run_coverage.sh
+++ run_coverage.sh
@@ -1,1 +1,8 @@
+rm -rf docs/coverage/
 XDEBUG_MODE=coverage  ./vendor/bin/phpunit tests --coverage-filter src --coverage-html docs/coverage
+mv docs/coverage/_css docs/coverage/phpunit_css
+mv docs/coverage/_icons docs/coverage/phpunit_icons
+mv docs/coverage/_js docs/coverage/phpunit_js
+grep -rl _css docs/coverage | xargs sed -i "" -e 's/_css/phpunit_css/g'
+grep -rl _icons docs/coverage | xargs sed -i "" -e 's/_icons/phpunit_icons/g'
+grep -rl _js docs/coverage | xargs sed -i "" -e 's/_js/phpunit_js/g'
